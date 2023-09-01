b8baca1d6e403127dfc63de8bdd111eb67a01a11
WorthOnGithub <119037611+WorthOnGithub@users.noreply.github.com>
2023-09-01 02:50:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,44 +2,67 @@ import pytesseract
 import cv2
 from pytesseract import Output
 
-# Configuration
-myconfig = r'--psm 11 --oem 3'
+# Constants and Parameters
+IMAGE_PATH = 'IMAGE_NAME'
+CONFIDENCE_THRESHOLD = 60
+PYTESSERACT_CONFIG = r'--psm 11 --oem 3'
 
-# Load and process the image
-img = cv2.imread('logos.png')
-height, width, _ = img.shape
 
-data = pytesseract.image_to_data(img, config=myconfig, output_type=Output.DICT)
-amount_boxes = len(data['text'])
+def main():
+    img = load_image(IMAGE_PATH)
+    data = extract_text_data(img, PYTESSERACT_CONFIG)
+    draw_text_boxes(img, data, CONFIDENCE_THRESHOLD)
+    lines = extract_text_lines(data)
+    result = '\n\n'.join(lines)
+    print(result)
+    display_image_with_boxes(img)
 
-# Draw boxes around text
-for i in range(amount_boxes):
-    if int(data['conf'][i]) > 60:
-        (x, y, w, h) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
-        img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        img = cv2.putText(img, data['text'][i], (x, y + h + 20), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
-# Extract text lines
-lines = []
-prev_top = None
+def load_image(image_path):
+    return cv2.imread(image_path)
 
-for i in range(len(data['text'])):
-    if int(data['conf'][i]) > 0:  # Filter out low-confidence results
-        left, top, w, h = data['left'][i], data['top'][i], data['width'][i], data['height'][i]
-        text = data['text'][i]
 
-        if text.strip():  # Skip empty text
-            if prev_top is None or top - prev_top >= h:  # New line
-                lines.append(text)
-            else:
-                lines[-1] += ' ' + text  # Append to the last line
+def extract_text_data(img, config):
+    return pytesseract.image_to_data(img, config=config, output_type=Output.DICT)
 
-            prev_top = top
 
-result = '\n'.join(lines)
-print(result)
+def draw_text_boxes(img, data, confidence_threshold):
+    for i in range(len(data['text'])):
+        conf = int(data['conf'][i])
 
-# Display the image with highlighted text boxes
-cv2.imshow('img', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+        if conf > confidence_threshold:
+            x, y, w, h = data['left'][i], data['top'][i], data['width'][i], data['height'][i]
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(img, data['text'][i], (x, y + h + 20), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+
+def extract_text_lines(data):
+    lines = []
+    prev_top = None
+
+    for i in range(len(data['text'])):
+        conf = int(data['conf'][i])
+
+        if conf > 0:  # Filter out low-confidence results
+            left, top, w, h = data['left'][i], data['top'][i], data['width'][i], data['height'][i]
+            text = data['text'][i]
+
+            if text.strip():  # Skip empty text
+                if prev_top is None or top - prev_top >= h:  # New line
+                    lines.append(text)
+                else:
+                    lines[-1] += ' ' + text  # Append to the last line
+
+                prev_top = top
+
+    return lines
+
+
+def display_image_with_boxes(img):
+    cv2.imshow('img', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
